fea481a950106aa5789a53baa0606c692af0c1fa
devJ666 <tranthaiquyen@vnpt.vn>
2026-03-03 14:03:21 +0700

    feat: Configure NFC foreground dispatch and technology filtering in the Android example application.

--- example/android/app/src/main/kotlin/com/example/flutter_plugin_nfc_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_plugin_nfc_example/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.vnpt.flutter_plugin_ic_nfc_example
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onResume() {
+        super.onResume()
+        val intent = Intent(context, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        NfcAdapter.getDefaultAdapter(context)?.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        NfcAdapter.getDefaultAdapter(context)?.disableForegroundDispatch(this)
+    }
+}
